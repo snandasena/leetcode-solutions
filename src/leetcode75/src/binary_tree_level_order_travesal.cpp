8805bf9a998d6be9a8670241da3c4a31ca786d64
Sajith Nandasena <sajithgithub@gmail.com>
2022-07-25 12:20:25 +0530

    Improved

--- src/leetcode75/src/binary_tree_level_order_travesal.cpp
+++ src/leetcode75/src/binary_tree_level_order_travesal.cpp
@@ -9,6 +9,36 @@ class Solution
 public:
     vector<vector<int>> levelOrder(TreeNode *root)
     {
+        vector<vector<int>> ans;
+        if (root == nullptr) return ans;
 
+        queue<TreeNode *> que;
+        que.push(root);
+        while (!que.empty())
+        {
+            auto sz = que.size();
+            vector<int> curr;
+            for (int i = 0; i < sz; ++i)
+            {
+                auto node = que.front();
+                que.pop();
+
+                if (node != nullptr)
+                {
+                    curr.push_back(node->val);
+                    if (node->left != nullptr)
+                    {
+                        que.push(node->left);
+                    }
+
+                    if (node->right)
+                    {
+                        que.push(node->right);
+                    }
+                }
+            }
+            ans.push_back(curr);
+        }
+        return ans;
     }
 };
