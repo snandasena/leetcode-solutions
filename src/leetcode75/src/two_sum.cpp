785696c4abead63e990bc9826271dea20b587e9e
Sajith Nandasena <sajithgithub@gmail.com>
2022-08-01 17:29:46 +0530

    Improved

--- src/leetcode75/src/two_sum.cpp
+++ src/leetcode75/src/two_sum.cpp
@@ -7,8 +7,33 @@
 class Solution
 {
 public:
+    vector<int> twoSum2(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            mp[nums[i]] = i;
+        }
+        int complement;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            complement = target - nums[i];
+            if (mp.count(complement) > 0 && mp[complement] != i)
+            {
+                return {i, mp[complement]};
+            }
+        }
+        return {-1, -1};
+    }
+
     vector<int> twoSum(vector<int> &nums, int target)
     {
+        unordered_map<int, int> mp;
+        vector<int> ans;
+
 
     }
+
+
 };
