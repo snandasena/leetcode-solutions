4c9e66d63b64c2a67bec37e1f30d26dc86db1380
Sajith Nandasena <sajithgithub@gmail.com>
2022-08-01 18:02:17 +0530

    Improved

--- src/leetcode75/src/two_sum.cpp
+++ src/leetcode75/src/two_sum.cpp
@@ -32,8 +32,19 @@ public:
         unordered_map<int, int> mp;
         vector<int> ans;
 
-
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (mp.find(target - nums[i]) != mp.end())
+            {
+                ans.push_back(i);
+                ans.push_back(mp[target - nums[i]]);
+                break;
+            }
+            else
+            {
+                mp[nums[i]] = i;
+            }
+        }
+        return ans;
     }
-
-
 };
