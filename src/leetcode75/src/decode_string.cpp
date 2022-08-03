07c5edcdd926223962ce7a649eb7998fa9498448
Sajith Nandasena <sajithgithub@gmail.com>
2022-08-03 17:17:04 +0530

    Improved

--- src/leetcode75/src/decode_string.cpp
+++ src/leetcode75/src/decode_string.cpp
@@ -7,28 +7,39 @@
 
 class Solution
 {
+    int i = 0;
+    string helper(string &str)
+    {
+        string res;
+        while (i < str.size() && str[i] != ']')
+        {
+            if (isdigit(str[i]))
+            {
+                int k = 0;
+                while (i < str.size() && isdigit(str[i]))
+                {
+                    k = k * 10 + str[i] - '0';
+                    ++i;
+                }
+                ++i;
+                string r = helper(str, i);
+                while (k-- > 0)
+                {
+                    res += r;
+                }
+                ++i;
+            }
+            else
+            {
+                res += str[i];
+                ++i;
+            }
+        }
+        return res;
+    }
 public:
     string decodeString(string s)
     {
-        stack<string> st;
-        int num;
-        int i = 0;
-        auto N = static_cast<int >(s.size());
-        while (i < N)
-        {
-            string curr;
-            while (isdigit(s[i]))
-            {
-                curr += s[i];
-                ++i;
-            }
-            if (!curr.empty())
-            {
-                st.push(curr);
-            }
-            curr.clear();
-
-            if()
-        }
+        return helper(s);
     }
 };
