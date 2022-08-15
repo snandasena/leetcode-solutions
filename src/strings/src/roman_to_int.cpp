f8ec4aef3b215a65b162c27d04e49ffbb498ca72
Sajith Nandasena <sajithgithub@gmail.com>
2022-08-15 13:42:41 +0530

    Improved

---
+++ src/strings/src/roman_to_int.cpp
@@ -0,0 +1,38 @@
+
+//
+// Created by sajith on 8/15/22.
+//
+
+#include "base.h"
+
+class Solution
+{
+    unordered_map<char, int> mp{
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000},
+    };
+
+public:
+    int romanToInt(string s)
+    {
+        int ans = 0;
+        for (auto i = 0; i < s.size() - 1; ++i)
+        {
+            if (mp[s[i]] >= mp[s[i + 1]])
+            {
+                ans += mp[s[i]];
+            }
+            else
+            {
+                ans -= mp[s[i]];
+            }
+        }
+        ans += mp[s.back()];
+        return ans;
+    }
+};
