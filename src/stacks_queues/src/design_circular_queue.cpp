529880eecadad4943056880b1ccae57a3d435854
Sajith Nandasena <sajithgithub@gmail.com>
2022-09-25 15:25:14 +0530

    Improved

--- src/stacks_queues/src/design_circular_queue.cpp
+++ src/stacks_queues/src/design_circular_queue.cpp
@@ -6,7 +6,7 @@
 
 class MyCircularQueue
 {
-    int cnt = 0;
+    int cnt;
     list<int> Q;
 public:
     MyCircularQueue(int k) : cnt{k}
@@ -16,31 +16,67 @@ public:
 
     bool enQueue(int value)
     {
-        if(cnt)
+        if (cnt > Q.size())
+        {
+            Q.push_back(value);
+            return true;
+        }
+        return false;
     }
 
     bool deQueue()
     {
-
+        if (!Q.empty())
+        {
+            Q.pop_front();
+            return true;
+        }
+        return false;
     }
 
     int Front()
     {
+        if (Q.empty())
+        {
+            return -1;
+        }
 
+        return Q.front();
     }
 
     int Rear()
     {
-
+        if (Q.empty())
+        {
+            return -1;
+        }
+        return Q.back();
     }
 
     bool isEmpty()
     {
-
+        return Q.empty();
     }
 
     bool isFull()
     {
-
+        return Q.size() == cnt;
     }
 };
+
+
+int main()
+{
+    MyCircularQueue myCircularQueue{3};
+    myCircularQueue.enQueue(1); // return True
+    myCircularQueue.enQueue(2); // return True
+    myCircularQueue.enQueue(3); // return True
+    myCircularQueue.enQueue(4); // return False
+    myCircularQueue.Rear();     // return 3
+    myCircularQueue.isFull();   // return True
+    myCircularQueue.deQueue();  // return True
+    myCircularQueue.enQueue(4); // return True
+    myCircularQueue.Rear();
+
+    return 0;
+}
