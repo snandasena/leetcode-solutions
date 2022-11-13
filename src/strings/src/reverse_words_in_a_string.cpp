d553f21d64a83686ff9741a5c3fb20b66397a621
Sajith Nandasena <sajithgithub@gmail.com>
2022-11-13 18:25:15 +0530

    Improved

---
+++ src/strings/src/reverse_words_in_a_string.cpp
@@ -0,0 +1,24 @@
+//
+// Created by sajith on 11/13/22.
+//
+class Solution
+{
+public:
+    string reverseWords(string s)
+    {
+        string res;
+        int i = 0, j;
+        while (i < s.length())
+        {
+            while (i < s.length() && s[i] == ' ') ++i;
+            if (i >= s.length()) break;
+            j = i + 1;
+            while (j < s.length() && s[j] != ' ') ++j;
+
+            if (res.empty()) res = s.substr(i, j - i);
+            else res = s.substr(i, j - i) + " " + res;
+            i = j + 1;
+        }
+        return res;
+    }
+};
