9b55a71099d19e3edd0d83201573ef0cd4bf8b0f
Sajith Nandasena <sajithgithub@gmail.com>
2022-12-06 21:51:17 +0530

    Improved

---
+++ src/linkedlist/src/odd_even_linkedlist.cpp
@@ -0,0 +1,27 @@
+//
+// Created by sajith on 12/6/22.
+//
+
+
+#include "base.h"
+
+class Solution
+{
+public:
+    ListNode *oddEvenList(ListNode *head)
+    {
+
+    }
+};
+
+int main()
+{
+    ListNode *root = new ListNode{1};
+    root->next->next = new ListNode{2};
+    root->next->next->next = new ListNode{3};
+    root->next->next->next->next = new ListNode{4};
+    root->next->next->next->next->next = new ListNode{5};
+
+    Solution sol;
+
+}
