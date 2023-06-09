4ba41fadcabdea0544e23582c18ed5b082ce3554
snandasena <sajithgithub@gmail.com>
2023-06-09 15:53:58 +0200

    Improved

--- src/2023/arrays/src/find_smallest_letter_greater_than_target.cpp
+++ src/2023/arrays/src/find_smallest_letter_greater_than_target.cpp
@@ -7,14 +7,13 @@ class Solution
 public:
     char nextGreatestLetter(vector<char> &letters, char target)
     {
-        auto res = find_if(letters.cbegin(), letters.cend(), [&target](auto c)
+        if(letters.back() <= target )
+            return letters[0];
+
+        return *find_if(letters.cbegin(), letters.cend(), [&target](auto c)
         {
             return c > target;
         });
-        if (res != letters.cend())
-            return *res;
-        else
-            return letters[0];
     }
 };
 
