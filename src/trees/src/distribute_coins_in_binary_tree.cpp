2c435e9256ea1994bfe9105237016394f3dcacd6
snandasena <sajithgithub@gmail.com>
2024-05-18 22:00:26 +0200

    M  src/trees/src/distribute_coins_in_binary_tree.cpp

--- src/trees/src/distribute_coins_in_binary_tree.cpp
+++ src/trees/src/distribute_coins_in_binary_tree.cpp
@@ -3,22 +3,24 @@
 //
 
 #include "base.h"
-class Solution {
+
+class Solution
+{
     int dfs(TreeNode *root, int &ans)
     {
-        if(root == nullptr)
+        if (root == nullptr)
             return 0;
 
         int left = dfs(root->left, ans);
         int right = dfs(root->right, ans);
         ans += abs(left) + abs(right);
 
-        return root->val -1 + left + right;
+        return root->val - 1 + left + right;
     }
 
 public:
 
-    int distributeCoins(TreeNode* root)
+    int distributeCoins(TreeNode *root)
     {
         int ans = 0;
         dfs(root, ans);
